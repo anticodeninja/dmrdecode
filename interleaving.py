07f62ad0e38069b05ea5379ca369f4a8d8cd6506
anticode.ninja <me@anticode.ninja>
2019-04-05 22:45:18 +0700

    Add license headers and shebangs

--- interleaving.py
+++ interleaving.py
@@ -1,3 +1,11 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This Source Code Form is subject to the terms of the
+# Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed
+# with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+# Copyright 2019 Artem Yamshanov, me [at] anticode.ninja
+
 BPTC_INTERLEAVING = [x * 181 % 196 for x in range(196)]
 CACH_INTERLEAVING = [0, 4, 8, 12, 14, 18, 22, 1, 2, 3, 5, 6, 7, 9, 10, 11, 13, 15, 16, 17, 19, 20, 21, 23]
 CACH_BPTC_INTERLEAVING = [(x * 4 % 68) + (x * 4 // 68) for x in range(68)]
